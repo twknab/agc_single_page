66b92f8eed22da73d3bb8eeed591877df16e83f3
twknab <20636750+twknab@users.noreply.github.com>
2018-08-15 18:45:21 -0700

    video fine tuned but thumbnail and play button still needs to be adjusted--was having trouble finding correct avenues in documentation and contacted wistia to see if I can get a little support

--- client/js/main.js
+++ client/js/main.js
@@ -4,7 +4,7 @@
 
 $(document).ready(function () {
 
-  // Get REST API data for Company Number and Hours:
+  // Get REST API data for Company Number and Hours - decides whether or not to show Call Me button (and converts user time to PST):
   let data = $.get( "https://www.algaecal.com/wp-json/acf/v3/options/options", function(info) {
 
     // Store important variables for determining if business is open and if to display phone number:
@@ -21,7 +21,65 @@ $(document).ready(function () {
     } else {
       $('#contact-wrapper').hide();
     }
-
-
   });
+
+
+  // Hide Pricing Boxes by Default:
+  $('#pricing-boxes-wrapper').hide();
+
+  // Get Wistia Video
+  window._wq = window._wq || [];
+  window._wq.push({
+    id: "cecdwaq3dz",
+    // video_thumbnail: "https://embed-ssl.wistia.com/deliveries/1a92364950dc0031a7ec983870ddce20b27582d0.jpg?video_still_time=292",
+    options: {
+      playerColor: "#4c813d",
+    },
+    onHasData: function (video) {
+      video.bind("play", function () {
+        console.log("'play' event fired for " + video.name() + "! 🎉");
+        return video.unbind;
+      });
+
+      // If second of video meets or exceeds 133 (2 minutes 13 seconds), pricing boxes will be display:
+      video.bind("secondchange", function (s) {
+        if (s >= 133) {
+          $('#pricing-boxes-wrapper').show();
+        }
+      });
+
+    }
+  });
+
+  $(video).attr("poster", "https://embed-ssl.wistia.com/deliveries/1a92364950dc0031a7ec983870ddce20b27582d0.jpg?video_still_time=292");
+
+
+  // var baseURL = "http://fast.wistia.com/oembed/?url=";
+  // var accountURL = escape("http://home.wistia.com/medias/");
+  // var mediaHashedID = "cecdwaq3dz";
+
+  // function getThumbnailUrl(hashedID, callback) {
+  //   $.get(baseURL + accountURL + hashedID + "&format=json&callback=?,", callback);
+  //   $.get("http://fast.wistia.net/oembed?url=http://home.wistia.com/medias/cecdwaq3dz?embedType=async&videoWidth=640", function (jsonObj) {
+  //     thumbnail_url = jsonObj.thumbnail_url + '&' + 'video_still_time=292';
+  //     console.log(thumbnail_url);
+  //   });
+
+  // }
+
+  // function parseJSON(json) {
+  //   // console.log(json.thumbnail_url);
+  //   console.log(json);
+  // };
+
+  // getThumbnailUrl(mediaHashedID, parseJSON);
+
+
+  // let url= "http://fast.wistia.net/oembed?url=https://embed-ssl.wistia.com/deliveries/1a92364950dc0031a7ec983870ddce20b27582d0.jpg?video_still_time=292"
+
+
+
+
+
+
 });
